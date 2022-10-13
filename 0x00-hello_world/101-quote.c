4eb63832309dd06c5924a569764c66684b5b5fa9
Yustino Nyendeza <ynyendeza@gmail.com>
2022-10-13 10:39:50 -0700

    UNIX is basically

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - prints a text
- *
- * Return: always 1 (Success)
+ * main - Prints a text
+ * return: Always 1
  */
 int main(void)
-
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar,
-2015-10-19,\n", 59);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
