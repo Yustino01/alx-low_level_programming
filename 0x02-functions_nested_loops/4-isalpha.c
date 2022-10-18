9ec91f49ba05c75de57bd03b00ed6a548bbb16cd
Yustino Nyendeza <ynyendeza@gmail.com>
2022-10-18 14:43:14 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isalpha - checks for the alphabets
  * @c: character to be checked
@@ -8,7 +9,7 @@
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-			return (1);
-		else
-			return (0);
+		return (1);
+	else
+		return (0);
 }
